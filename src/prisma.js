8cfbda66314325a04196870b2e5550382c196076
Lucia Castro Beilicke <lcastrobeilicke@gmail.com>
2026-03-27 22:58:49 -0300

    downgrade to prisma 5

--- src/prisma.js
+++ src/prisma.js
@@ -1,14 +1,5 @@
-require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
 
-let prisma;
-
-if (!global.prisma) {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-  global.prisma = new PrismaClient({ adapter });
-}
-
-prisma = global.prisma;
+const prisma = new PrismaClient();
 
 module.exports = prisma;
